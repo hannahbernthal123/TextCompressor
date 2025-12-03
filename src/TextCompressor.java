182087c0a3ca8bbe5e7b8d9a9b775726817fefe1
hannahbernthal <hannah.bernthal@menloschool.org>
2025-12-02 18:51:14 -0800

    first commit

--- src/TextCompressor.java
+++ src/TextCompressor.java
@@ -21,6 +21,9 @@
  *  = 43.54% compression ratio!
  ******************************************************************************/
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *  The {@code TextCompressor} class provides static methods for compressing
  *  and expanding natural language through textfile input.
@@ -29,9 +32,69 @@
  */
 public class TextCompressor {
 
+    // Two letter common words.
+    public static final int be = 1;
+    public static final int to = 2;
+    public static final int of = 3;
+    public static final int in = 6;
+    public static final int he = 14;
+    public static final int as = 15;
+    public static final int on = 12;
+    public static final int it = 10;
+    public static final int at = 17;
+
+    // Three letter
+    public static final int the = 0;
+    public static final int not = 11;
+    public static final int and = 4;
+    public static final int you = 16;
+
+    // Four letter
+    public static final int that = 7;
+    public static final int have = 8;
+    public static final int with = 13;
+
+
+
     private static void compress() {
 
-        // TODO: Complete the compress() method
+        String s = BinaryStdIn.readString();
+        int n = s.length();
+
+        BinaryStdOut.write(n);
+
+        // Write out each character
+//        for (int i = 0; i < n; i++) {
+//            if (i + 1 <= n && s.substring(i,i+1).equals("a")) {
+//                BinaryStdOut.write(a);
+//            }
+//            if (i + 1 <= n && s.substring(i,i+1).equals("I")) {
+//                BinaryStdOut.write(a);
+//            }
+
+
+
+//        }
+
+        String text = "hi i am testing if this even words";
+        String[] words = text.split(" ");
+
+        HashMap<String, Integer> counts = new HashMap<>();
+
+        for (int i = 0; i < words.length; i++) {
+            int count = 0;
+
+            // rescans entire list for each word
+            for (int j = 0; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    count++;
+                }
+            }
+
+            counts.put(words[i], count);
+        }
+
+        // Here I would theoretically make escape keys for the words that appeared a bunch.
 
         BinaryStdOut.close();
     }
@@ -48,5 +111,4 @@ public class TextCompressor {
         else if (args[0].equals("+")) expand();
         else throw new IllegalArgumentException("Illegal command line argument");
     }
-    //
 }
